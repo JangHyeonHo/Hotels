740620295d8c0e85efc66206329da675b2570e78
kow1000 <kow1000@gmail.com>
2019-03-15 17:15:41 +0900

    커밋

---
+++ src/main/java/com/hotels/peregrine/repository/fb/BanqeutRegisterRepository.java
@@ -0,0 +1,5 @@
+package com.hotels.peregrine.repository.fb;
+
+public class BanqeutRegisterRepository {
+
+}
---
+++ src/main/java/com/hotels/peregrine/service/fb/BanqeutRegisterService.java
@@ -0,0 +1,22 @@
+package com.hotels.peregrine.service.fb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hotels.peregrine.repository.fb.BanqeutRegisterRepository;
+
+@Service
+public class BanqeutRegisterService {
+
+	@Autowired
+	BanqeutRegisterRepository repository;
+	
+	
+	public void action() {
+		
+		
+		
+	}
+	
+	
+}
