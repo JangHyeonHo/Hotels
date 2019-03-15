b5f0fdbfc02ccbcc35a5550fd912a8ed9499c896
gusghwkd202 <gusghwkd202@naver.com>
2019-03-15 17:24:35 +0900

    c

--- src/main/java/com/hotels/peregrine/controller/logistics/ContractController.java
+++ src/main/java/com/hotels/peregrine/controller/logistics/ContractController.java
@@ -1,16 +1,22 @@
 package com.hotels.peregrine.controller.logistics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.hotels.peregrine.service.logistics.ContractListService;
+
 @Controller
 public class ContractController {
 	
+	@Autowired
+	private ContractListService service;
+	
 	@RequestMapping(value="/comp/contract", method = RequestMethod.GET)
 	public String regist(Model model) {
-		
+		service.ContractList();
 		return "logistics/contract/distriRegist";
 	}
 }
