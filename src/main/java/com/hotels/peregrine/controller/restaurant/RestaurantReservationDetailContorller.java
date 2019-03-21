a8224ba15b3e16c1f7508641c1a55a397a0ccd56
kow1000 <kow1000@gmail.com>
2019-03-21 14:10:17 +0900

    귀밋

---
+++ src/main/java/com/hotels/peregrine/controller/restaurant/RestaurantReservationDetailContorller.java
@@ -0,0 +1,33 @@
+package com.hotels.peregrine.controller.restaurant;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.hotels.peregrine.model.RestaurantReservationDTO;
+import com.hotels.peregrine.service.restaurant.RestaurantReservationDetailService;
+
+@Controller
+@RequestMapping("/comp/restaurant/reservation/list/detail")
+public class RestaurantReservationDetailContorller {
+
+	@Autowired
+	RestaurantReservationDetailService service;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getform(Model model , @RequestParam("rrno") long num) {
+		System.out.println("레스토랑 예약 상세정보 오픈");
+		
+		List<RestaurantReservationDTO> list= service.action(num);
+		
+		model.addAttribute("list",list);
+		
+		return "restaurant/restaurantReservationDetail";
+	}
+	
+}
